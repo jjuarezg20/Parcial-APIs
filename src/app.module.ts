402ef46d156b29f828d03d93c817fc4ebb2e8591
jjuarezg20 <20235076@esen.edu.sv>
2026-08-05 17:07:09 -0600

    refactor: load env vars via @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
 import { UsersModule } from './users/users.module';
@@ -9,6 +10,7 @@ import { Service } from './services/service.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
